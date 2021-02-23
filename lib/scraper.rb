4bee5e3fd8a77c55e3329a5b465128a354b24c17
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-23 02:19:54 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,7 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
 
-    cards = doc.css(".student-test-container")
+    cards = doc.css(".student-text-container")
 
     #students = doc.css(".student-name")
     #location = doc.css(".student-location")
