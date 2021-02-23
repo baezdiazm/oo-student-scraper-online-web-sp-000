0c1d4a0afa55f993fdc4b026ac2f7377d423a250
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-23 03:51:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -19,7 +19,7 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.bio = attributes_hash[:bio]
+    @bio = attributes_hash[:bio]
     @blog = attributes_hash[:blog]
   end
 
