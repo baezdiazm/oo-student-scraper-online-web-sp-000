155ac3666ca305fdac9cf1ad08f9c6956e3be8cf
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-23 03:43:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,7 @@ class Student
 
   def self.create_from_collection(students_array) ##Each element of this array is one hash, gotta iterate over this array to acept each indidual student, and #Initialize will grab their names and locations
 
-    students_array.each do |element|
+    students_array.each do |element| ## Now we're working with each hash and creating a new Student instance with the :Keys and "Values" of each hash.
       student = Student.new(element)
       student
     end
