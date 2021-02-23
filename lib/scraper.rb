04a20492a51a10ae71b7e335ee2b371565626de3
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-23 03:27:06 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -33,7 +33,7 @@ class Scraper
 
       hashy[:profile_quote] = doc.css(".vitals-container .vitals-text-container .profile-quote").text
 
-      hashy[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder.description-holder p").text
+      hashy[:bio] = doc.css(".bio-block.details-block .bio-content.content-holder .description-holder p").text
 
     end
     hashy
