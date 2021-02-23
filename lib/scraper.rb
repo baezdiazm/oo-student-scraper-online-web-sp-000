243dcf656f98195df0afc019c31619b231fcd60d
Manuel Eduardo Baez Diaz <baezdiazm@yahoo.com>
2021-02-23 02:38:01 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -9,7 +9,7 @@ class Scraper
 
     cards.collect do |item|
       { :name => item.css(".student-name").text,
-        :location => item.css(".student-location").text
+        :location => item.css(".student-location").text,
         :profile_url => item.css(".student-profile").text
       }
     end
